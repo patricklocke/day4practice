379889a7568a42d31e3e1fbd9e042c4789afed84
Patrick Locke <patrick.s.locke@gmail.com>
2016-02-25 10:02:56 -0600

    moved js to 'practice.js';

---
+++ practice.js
@@ -0,0 +1,36 @@
+
+function checkAge() {
+    var age = prompt("What is your age?");
+    console.log('Hello! You are ' + age + ' years old!');
+    if (age < 21) {
+        alert('You are under 21!');
+    }
+    else {
+        alert('You are old enough to enter!');
+    }
+
+}
+function checkGrade() {
+    var grade = prompt('What is your letter grade in your favorite class?');
+    grade = grade.toUpperCase();
+    switch (grade) {
+        case 'A':
+            alert('You are an excellent student!');
+            break;
+        case 'B':
+            alert('You are a good student!');
+            break;
+        case 'C':
+            alert('You can do better!');
+            break;
+        case 'D':
+            alert('You should study more!');
+            break;
+        case 'F':
+            alert('You need a tutor!');
+            break;
+        default:
+            alert("I'm sorry, I didn't understand you!");
+    }
+}    
+                
